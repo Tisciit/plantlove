e92a89cfb6f4d6fad79fb56b7d691896f32bc11c
Philipp <tisciit@gmail.com>
2019-04-14 15:17:16 +0200

    initialization of project

---
+++ src/components/plant.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Plant extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div>
+                <span>I am a plant</span>
+            </div>
+        );
+    }
+}
+ 
+export default Plant;

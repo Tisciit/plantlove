cc46b7c41edcc84b6455a3d9ef41902c62c123c9
Philipp <tisciit@gmail.com>
2019-04-25 21:04:07 +0200

    "fixed" adding of bugs by wrapping add-buttons onclick to another function

--- src/components/plants.jsx
+++ src/components/plants.jsx
@@ -5,7 +5,7 @@ const Plants = () => {
 
     const [plants, setPlants] = useState([])
 
-    const addPlant = (name, description) => {
+    const addPlant = (name = "no name", description = "no desc") => {
         const _plants = [...plants];
         _plants.push({
             name: name,
@@ -14,13 +14,17 @@ const Plants = () => {
         setPlants(_plants);
     }
 
+    const showAddPlant = () => {
+        addPlant("","");
+    }
+
     return (
         <div>
             {plants.map((p, index) => {
                 return <Plant key={index} name={p.name} description={p.description} />
             })}
             <div className="button-row">
-                <button className="button-round-add" onClick={addPlant}>Add</button>
+                <button className="button-round-add" onClick={showAddPlant}>Add</button>
             </div>
         </div>
     )

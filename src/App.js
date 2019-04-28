2a3433b5063729395f328f9b991f37628518175b
Philipp <tisciit@gmail.com>
2019-04-28 21:01:23 +0200

    svgAnimation: switched to react class to be able to use componentdidmount

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
 import Plants from './components/plants'
+import SVGAnimation from './components/svgAnimation'
 
 const App = () => {
   
@@ -9,8 +10,8 @@ const App = () => {
       {/* <Plant name="Efeutute" description="Schrank Wohnzimmer"/>
       <Plant name="Hängi" description="Über Fernseher links"/>
       <Plant name="Hängi" description="Über Fernseher rechts"/> */}
-
-      <Plants />
+      <SVGAnimation />
+     {/* <Plants /> */}
     </div>
   );
 }

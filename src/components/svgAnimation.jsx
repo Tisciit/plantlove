429c88608f2fcb4a71e288d029c5653421b8d948
Philipp <tisciit@gmail.com>
2019-05-05 12:43:42 +0200

    changed svgAnimation back to functional component

--- src/components/svgAnimation.jsx
+++ src/components/svgAnimation.jsx
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React, { useEffect } from 'react';
 import style from './svgAnimation.module.css'
 
-class SVGAnimation extends Component {
+const SVGAnimation = (props) => {
 
-    setCSS() {
+    const setCSS = () => {
         const paths = document.querySelectorAll(`svg path`);
         let animationOffSet = 0;
         const animationOffSetIncrement = .01;
@@ -25,17 +25,17 @@ class SVGAnimation extends Component {
         }
     }
 
-    componentDidMount() {
-        this.setCSS();
-    }
+    //run setCSS on load
+    useEffect(() => {
+        setCSS();
+    });
 
-    render() {
-        return (
-            <div className={style.center}>
-                {this.props.children}
-            </div>
-        );
-    }
+    return (
+        <div className={style.center}>
+            {console.log(props)}
+            {props.children}
+        </div>
+    );
 }
 
 export default SVGAnimation;

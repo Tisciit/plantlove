e22e10c0c8e1d2793e8a5683d67fff83190f958d
Philipp <tisciit@gmail.com>
2019-05-08 21:34:34 +0200

    Navbar and Styling added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
+import Style from './Navbar.module.css'
 
 const Navbar = () => {
 
+    const toggleSlide = () => {
+        document.querySelector(`.${Style.navBurgerTop}`).classList.toggle(Style.navBurgerTopRotated);
+        document.querySelector(`.${Style.navBurgerMiddle}`).classList.toggle(Style.navBurgerMiddleHidden);
+        document.querySelector(`.${Style.navBurgerBottom}`).classList.toggle(Style.navBurgerBottomRotated);
+
+        document.querySelector(`.${Style.navLinks}`).classList.toggle(Style.navLinksSlide);
+    }
+
     return (
-        <nav className="navbar" style={{backgroundColor: "#76a912"}}>
-            <img className = "img-cirlce" src='favicon.ico' alt="plantlove"></img>
-            <ul>
-                <li>Settings</li>
-                <li>About</li>
+        <div className={Style.nav}>
+            <div className={Style.navLogo}><h3>LOGO</h3></div>
+            <ul className={Style.navLinks}>
+                <li> <a href="#">About</a></li>
+                <li> <a href="#">Shop</a></li>
+                <li> <a href="#">Stuff</a></li>
+                <li> <a href="#">Things</a></li>
             </ul>
-        </nav>
+            <div className={Style.navBurger} onClick={toggleSlide}>
+                <div className={Style.navBurgerTop}></div>
+                <div className={Style.navBurgerMiddle}></div>
+                <div className={Style.navBurgerBottom}></div>
+            </div>
+        </div>
     );
 }
 

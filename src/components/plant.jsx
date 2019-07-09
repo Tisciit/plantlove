112dab643c9d719159b4d1a492c17cb886f84744
Philipp <tisciit@gmail.com>
2019-07-09 19:17:27 +0200

    changed basic styling

--- src/components/plant.jsx
+++ src/components/plant.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './Plant.module.css'
 import image from '../plant.svg'
+import check from '../baseline-done-24px.svg';
 
 const Plant = ({ name, description }) => {
 
-    //const [nextW, setNextW] = useState(0);
+    const [selected, setSelected] = useState(false);
 
-    const showContext = () => {
-        console.log("context");
+    const toggleSelection = (e) => {
+        console.log("click");
+        e.preventDefault();
+        setSelected(!selected);
     }
 
     return (
         <div className={style.div}>
-            <img className={style.imgCircle} src={image} onContextMenu={showContext} alt="Plants here :("></img>
+            <img className={style.imgCircle} src={!selected ? image : check} onClick={toggleSelection} alt="Plants here :("></img>
             <h2 className={style.plantName}>{name}</h2>
             <p className={style.description}>{description}</p>
         </div>

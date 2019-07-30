bafab4bc75285d25bbc011723ec57d2667c574ea
Tisciit <38668741+Tisciit@users.noreply.github.com>
2019-07-30 14:27:56 +0200

    renamed files / added additional idb wrappers

--- src/idb.js
+++ src/idb.js
@@ -1,92 +1,118 @@
-//Setup local Storage
+//Setup indexedDB constants
 export const DB_NAME = "plantlove";
 export const DB_VERSION = 1;
 export const PLANT_DB = "plants";
 export const SETTINGS_DB = "settings";
 
-
 export const idbConnect = () => {
-    return new Promise((resolve, reject) => {
-        const request = indexedDB.open(DB_NAME, DB_VERSION)
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(DB_NAME, DB_VERSION);
 
-        request.onsuccess = ev => {
-            resolve(ev.target);
-        }
+    request.onsuccess = ev => {
+      resolve(ev.target);
+    };
 
-        request.onupgradeneeded = ev => {
-            const db = ev.target.result;
+    request.onupgradeneeded = ev => {
+      const db = ev.target.result;
 
-            //set up the plant store
-            const plantStore = db.createObjectStore(PLANT_DB, {
-                autoIncrement: true
-            });
-            //plantStore.createIndex("Name","displayName",{unique: false});
+      //set up the plant store
+      const plantStore = db.createObjectStore(PLANT_DB, {
+        autoIncrement: true
+      });
+      //plantStore.createIndex("Name","displayName",{unique: false});
 
-            //set up the settings store
-        }
+      //set up the settings store
+      const settingStore = db.createObjectStore(SETTINGS_DB, {
+        keyPath: "title"
+      });
+    };
 
-        request.onerror = ev => {
-            reject(`IDB ERROR: ${ev}`);
-        }
+    request.onerror = ev => {
+      reject(`IDB ERROR: ${ev}`);
+    };
 
-        request.onblocked = ev => {
-            reject(`IDB BLOCKED: ${ev}`)
-        }
-    });
-}
+    request.onblocked = ev => {
+      reject(`IDB BLOCKED: ${ev}`);
+    };
+  });
+};
 
 export const idb = idbConnect();
 
 export const idbAddItem = (db, item) => {
-    return new Promise((resolve, reject) => {
-        const objectStore = idb.transaction(db, "readwrite").objectStore(db);
-        const request = objectStore.add(item);
+  return new Promise((resolve, reject) => {
+    const objectStore = idb.transaction(db, "readwrite").objectStore(db);
+    const request = objectStore.add(item);
 
-        request.onsuccess = ev => {
-            resolve(`Item ${ev.target.result} added to ${db}`);
-        }
+    request.onsuccess = ev => {
+      resolve(`Item ${ev.target.result} added to ${db}`);
+    };
 
-        request.onerror = ev => {
-            reject(`An error occured: ${ev}`)
-        }
-    });
-}
+    request.onerror = ev => {
+      reject(`An error occured: ${ev}`);
+    };
+  });
+};
 
 export const idbRemoveItem = (db, key) => {
-    return new Promise((resolve, reject) => {
-        const objectStore = idb.transaction(db, "readwrite").objectStore(db);
-        const request = objectStore.remove(key);
+  return new Promise((resolve, reject) => {
+    const objectStore = idb.transaction(db, "readwrite").objectStore(db);
+    const request = objectStore.remove(key);
 
-        request.onsuccess = ev => {
-            resolve(`Item ${ev.target.result} removed from ${db}`);
+    request.onsuccess = ev => {
+      resolve(`Item ${ev.target.result} removed from ${db}`);
+    };
+
+    request.onerror = ev => {
+      reject(`An error occured: ${ev}`);
+    };
+  });
+};
+
+export const idbGetAllItems = db => {
+  return new Promise((resolve, reject) => {
+    const objectStore = idb.transaction(db, "read").objectStore(db);
+
+    const data = [];
+    const cursor = objectStore.openCursor();
+
+    cursor.onsuccess = e => {
+      const cursor = e.target.result;
+      if (cursor) {
+        data.push(cursor.value);
+        cursor.continue();
+      } else {
+        resolve(data);
+      }
+    };
+
+    cursor.onerror = e => {
+      reject(e);
+    };
+  });
+};
+
+export const idbGetMatchingItems = (db, attr, key) => {
+  return new Promise((resolve, reject) => {
+    const objectStore = idb.transaction(db, "read").objectStore(db);
+
+    const data = [];
+    const cursor = objectStore.openCursor();
+
+    cursor.onsuccess = e => {
+      const cursor = e.target.result;
+      if (cursor) {
+        if (cursor.value[attr] === key) {
+          data.push(cursor.value);
         }
+        cursor.continue();
+      } else {
+        resolve(data);
+      }
+    };
 
-        request.onerror = ev => {
-            reject(`An error occured: ${ev}`)
-        }
-    });
-}
-
-export const idbGetAllItems = (db) => {
-
-    return new Promise((resolve, reject) => {
-        const objectStore = idb.transaction(db, "read").objectStore(db);
-        
-        const data = [];
-        const cursor = objectStore.openCursor();
-        
-        cursor.onsuccess = e => {
-          const cursor = e.target.result;
-          if (cursor) {
-            data.push(cursor.value);
-            cursor.continue();
-          } else {
-            resolve(data);
-          }
-        };
-
-        cursor.onerror = e => {
-            reject(e);
-        }
-    });
-}
+    cursor.onerror = e => {
+      reject(e);
+    };
+  });
+};
--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+
+import { idb } from "./idb";
 
 import rootReducer from "./store/reducers";
 import thunkMiddleWare from "redux-thunk";
-import {Provider} from "react-redux";
-import {createStore, applyMiddleware, compose} from "redux";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleWare)));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunkMiddleWare))
+);
 
 ReactDOM.render(
-    <Provider store={store}
-        ><App />
-    </Provider>, 
-    document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change

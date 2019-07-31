11732eae00fd939995c368565dfe5f4ccf2bfe1e
Philipp <tisciit@gmail.com>
2019-07-31 10:49:59 +0200

    App jsx added useEffect to load Data from idb

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,21 @@ import Plants from './Plants'
 import Navbar from './Navbar';
 import AddPlant from './AddPlant';
 import Settings from './Settings';
+import {useEffect} from "react";
+import {idbGetDefaultPlants} from "../store/actions";
+import {useDispatch} from "react-redux";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
+  
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    //This will run once the app loads. It´s the perfect time to update our App States from indexedDB
+
+    dispatch(idbGetDefaultPlants());
+  },[]);
   return (
     <Router>
       <div className="App">

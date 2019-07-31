8a7aedadee641fbaad8508f1205c5ea7c90b4e57
Tisciit <38668741+Tisciit@users.noreply.github.com>
2019-07-31 14:51:48 +0200

    fixed idb.js to actually work

--- src/idb.js
+++ src/idb.js
@@ -3,14 +3,14 @@ export const DB_NAME = "plantlove";
 export const DB_VERSION = 1;
 export const PLANT_DB = "plants";
 export const SETTINGS_DB = "settings";
-export const DEFAULT_PLANTS_DB = "defaultPlants"
+export const DEFAULT_PLANTS_DB = "defaultPlants";
 
-export const idbConnect = () => {
+const idbConnect = () => {
   return new Promise((resolve, reject) => {
     const request = indexedDB.open(DB_NAME, DB_VERSION);
 
     request.onsuccess = ev => {
-      resolve(ev.target);
+      resolve(ev.target.result);
     };
 
     request.onupgradeneeded = ev => {
@@ -27,7 +27,9 @@ export const idbConnect = () => {
         keyPath: "title"
       });
 
-      const defaultPlants = db.createObjectStore(DEFAULT_PLANTS_DB, {autoIncrement: true});
+      const defaultPlants = db.createObjectStore(DEFAULT_PLANTS_DB, {
+        autoIncrement: true
+      });
     };
 
     request.onerror = ev => {
@@ -43,79 +45,88 @@ export const idbConnect = () => {
 export const idb = idbConnect();
 
 export const idbAddItem = (db, item) => {
-  return new Promise((resolve, reject) => {
-    const objectStore = idb.transaction(db, "readwrite").objectStore(db);
-    const request = objectStore.add(item);
+  return idb.then(idb => {
+    return new Promise((resolve, reject) => {
+      const objectStore = idb.transaction(db, "readwrite").objectStore(db);
+      const request = objectStore.add(item);
 
-    request.onsuccess = ev => {
-      resolve(`Item ${ev.target.result} added to ${db}`);
-    };
+      request.onsuccess = ev => {
+        resolve(`Item ${ev.target.result} added to ${db}`);
+      };
 
-    request.onerror = ev => {
-      reject(`An error occured: ${ev}`);
-    };
+      request.onerror = ev => {
+        reject(`An error occured: ${ev}`);
+      };
+    });
   });
 };
 
 export const idbRemoveItem = (db, key) => {
-  return new Promise((resolve, reject) => {
-    const objectStore = idb.transaction(db, "readwrite").objectStore(db);
-    const request = objectStore.remove(key);
+  return idb.then(idb => {
+    return new Promise((resolve, reject) => {
+      const objectStore = idb.transaction(db, "readwrite").objectStore(db);
+      const request = objectStore.remove(key);
 
-    request.onsuccess = ev => {
-      resolve(`Item ${ev.target.result} removed from ${db}`);
-    };
+      request.onsuccess = ev => {
+        resolve(`Item ${ev.target.result} removed from ${db}`);
+      };
 
-    request.onerror = ev => {
-      reject(`An error occured: ${ev}`);
-    };
+      request.onerror = ev => {
+        reject(`An error occured: ${ev}`);
+      };
+    });
   });
 };
 
 export const idbGetAllItems = db => {
-  return new Promise((resolve, reject) => {
-    const objectStore = idb.transaction(db, "read").objectStore(db);
+  return idb.then(idb => {
+    console.log(idb);
+    return new Promise((resolve, reject) => {
+      const objectStore = idb.transaction(db, "readonly").objectStore(db);
 
-    const data = [];
-    const cursor = objectStore.openCursor();
+      const data = [];
+      const cursor = objectStore.openCursor();
 
-    cursor.onsuccess = e => {
-      const cursor = e.target.result;
-      if (cursor) {
-        data.push(cursor.value);
-        cursor.continue();
-      } else {
-        resolve(data);
-      }
-    };
+      cursor.onsuccess = e => {
+        const cursor = e.target.result;
+        if (cursor) {
+          data.push(cursor.value);
+          cursor.continue();
+        } else {
+          resolve(data);
+        }
+      };
 
-    cursor.onerror = e => {
-      reject(e);
-    };
+      cursor.onerror = e => {
+        reject(e);
+      };
+    });
   });
 };
 
 export const idbGetMatchingItems = (db, attr, key) => {
-  return new Promise((resolve, reject) => {
-    const objectStore = idb.transaction(db, "read").objectStore(db);
+  return idb.then(idb => {
+    return new Promise((resolve, reject) => {
+      const objectStore = idb.transaction(db, "read").objectStore(db);
 
-    const data = [];
-    const cursor = objectStore.openCursor();
+      const data = [];
+      const cursor = objectStore.openCursor();
 
-    cursor.onsuccess = e => {
-      const cursor = e.target.result;
-      if (cursor) {
-        if (cursor.value[attr] === key) {
-          data.push(cursor.value);
+      cursor.onsuccess = e => {
+        const cursor = e.target.result;
+        if (cursor) {
+          if (cursor.value[attr] === key) {
+            data.push(cursor.value);
+          }
+          cursor.continue();
+        } else {
+          resolve(data);
         }
-        cursor.continue();
-      } else {
-        resolve(data);
-      }
-    };
+      };
 
-    cursor.onerror = e => {
-      reject(e);
-    };
+      cursor.onerror = e => {
+        reject(e);
+      };
+    });
   });
 };
